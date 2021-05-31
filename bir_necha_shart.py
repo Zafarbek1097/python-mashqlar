7755fe8566e8a2988c117988a4e79fdcf856dbc0
Zafar Nurmatov <zafarnurmatov03@gmail.com>
2021-05-31 23:59:12 +0500

    Update bir_necha_shart.py

--- bir_necha_shart.py
+++ bir_necha_shart.py
@@ -208,27 +208,30 @@ Created on Sat May 29 20:07:25 2021
  #   print("Savatingiz bo'sh")            
     
 
-mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
-               'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
-savat = []
-for n in range(5):
-    savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+##mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+#               'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+#savat = []
+#for n in range(5):
+ #   savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
 
-bor_mahsulotlar = []
-mavjud_emas = []
-for mahsulot in savat:
-    if mahsulot in mahsulotlar:
-        bor_mahsulotlar.append(mahsulot)
-    else:
-        mavjud_emas.append(mahsulot)
+#bor_mahsulotlar = []
+#mavjud_emas = []
+#for mahsulot in savat:
+ #   if mahsulot in mahsulotlar:
+  #      bor_mahsulotlar.append(mahsulot)
+   # else:
+    #    mavjud_emas.append(mahsulot)
 
-if mavjud_emas:
-  print("Do'konimizda quyidagi mahsulotlar yo'q:")
-for mahsulot in mavjud_emas:
-  print(mahsulot)
-else:
-  print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
-    
+#if mavjud_emas:
+ # print("Do'konimizda quyidagi mahsulotlar yo'q:")
+#for mahsulot in mavjud_emas:
+ # print(mahsulot)
+#else:
+ # print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
+  
+
+
+  
 
 
 
